a8a422948f1c43d955a771aa05870e3961585ace
takkiiiiiiiii <yudai2002brazil@gmail.com>
2022-04-02 19:58:38 +0900

    automation

---
+++ go.mod
@@ -0,0 +1,5 @@
+module takky
+
+go 1.18
+
+require github.com/sclevine/agouti v3.0.0+incompatible // indirect
---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/sclevine/agouti"
+)
+
+func main() {
+	// ブラウザはChromeを指定して起動
+	driver := agouti.ChromeDriver(agouti.Browser("chrome"))
+	if err := driver.Start(); err != nil {
+		log.Fatalf("Failed to start driver:%v", err)
+	}
+	defer driver.Stop()
+
+	page, err := driver.NewPage()
+	if err != nil {
+		log.Fatalf("Failed to open page:%v", err)
+	}
+	// ログインページに遷移
+	if err := page.Navigate("https://elms.u-aizu.ac.jp/login/index.php"); err != nil {
+		log.Fatalf("Failed to navigate:%v", err)
+	}
+
+	// ID, Passの要素を取得し、値を設定
+	identity := page.FindByID("username")
+	password := page.FindByID("password")
+	identity.Fill("s1290077")
+	password.Fill("takkiiiiiiiii25")
+	// formをサブミット
+	/*
+		if err := page.FindByClass("loginbtn.btn.btn-primary.btn-block.mt-3").Submit(); err != nil {
+			log.Fatalf("Failed to login:%v", err)
+		}
+	*/
+	btn := page.FindByButton("Log in")
+	if err = btn.Click(); err != nil {
+		log.Fatalf("Failed to navigate:%v", err)
+	}
+	// 処理完了後、3秒間ブラウザを表示しておく
+	time.Sleep(10 * time.Second)
+
+}
